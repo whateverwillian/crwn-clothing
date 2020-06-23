e3f544dd05348709457199f69e4e7abefd0646ec
Willian Tavares <willianliketavares@gmail.com>
2020-06-23 18:25:09 -0300

    started the collections page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component'
+import Shop from './pages/shop/shop.component';
 
 function App() {
   return (
     <BrowserRouter>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={Shop} />
+
+      </Switch>
     </BrowserRouter>
   );
 }
