74151dbbcbd85cdacefcf3a384cf20f45c53a480
Willian Tavares <willianliketavares@gmail.com>
2020-06-26 11:33:12 -0300

    added redux, rootReducer and userReducer

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,18 @@
+const initial = {
+  currentUser: null,
+}
+
+const userReducer = (state = initial, action = {}) => {
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        currentUser: action.payload,
+      }
+  
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
